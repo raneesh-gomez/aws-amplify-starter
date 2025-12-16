7bc6f40c7b0976f1c00913d94dc9850c9c1307c6
Raneesh Gomez <raneesh@calcey.com>
2025-12-16 11:38:59 +0530

    chore: minor cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
 
   return (
     <main>
-      s<h1>{user?.signInDetails?.loginId}'s todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
